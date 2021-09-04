334cbd0b5f105bae2729e34551e6adf427d765f1
Natthaphon Wichianphong <klaosiroth@gmail.com>
2021-09-04 07:58:03 +0700

    Created Routes and Controllers structure

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
+const tasks = require('./routes/tasks')
 
-// routes
-app.get('/hello', (req, res) => {
-  res.status(200).send('Task Manager App')
-})
+// middleware
+// app.use(express.static('./public'))
+app.use(express.json())
+
+app.use('/api/v1/tasks', tasks)
+
+// app.get('/api/v1/tasks')         - get all the task
+// app.post('/api/v1/tasks')        - create a new task
+// app.get('/api/v1/tasks/:id')     - get single task
+// app.patch('/api/v1/tasks/:id')   - update task
+// app.delete('/api/v1/tasks/:id')  - delete task
 
 const port = 3000
 
